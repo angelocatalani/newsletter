7dbfa4155accaf8866506e073b8f6e72bb4ef365
angelocat <catalaniangelo@gmail.com>
2021-03-18 19:22:03 +0100

    fix(ci): format files

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 use newsletter::run;
-use std::net::{SocketAddr, TcpListener};
-use socket2::{Socket, Domain, Type};
+use socket2::{
+    Domain,
+    Socket,
+    Type,
+};
+use std::net::{
+    SocketAddr,
+    TcpListener,
+};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     // Create a TCP listener bound to two addresses.
-    let socket = Socket::new(Domain::IPV6, Type::STREAM, None)?;
+    // let socket = Socket::new(Domain::IPV6, Type::STREAM, None)?;
     // todo: solve cargo audit warning
-    let address: SocketAddr = "[::1]:12345".parse().unwrap();
-    socket.bind(&address.into())?;
-    socket.set_only_v6(false)?;
-    socket.listen(128)?;
+    // let address: SocketAddr = "[::1]:12345".parse().unwrap();
+    // socket.bind(&address.into())?;
+    // socket.set_only_v6(false)?;
+    // socket.listen(128)?;
 
-    let listener: TcpListener = socket.into();
-
-
+    // let listener: TcpListener = socket.into();
 
     let tcp_listener = TcpListener::bind("127.0.0.1:8080").expect("tcp error binding to port");
     run(tcp_listener)?.await
-
 }
