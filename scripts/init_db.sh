56c4d80c66c67d92428d34ff1b94666f368b4492
angelocat <catalaniangelo@gmail.com>
2021-03-20 12:41:04 +0100

    feat(db): wait postgress is ready before creating the migration

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
+
 # fails if any command fails
 set -x
 # fails if any pipe fails
@@ -12,15 +13,22 @@ NEWSLETTER_DB_NAME="${NEWSLETTER_DB_NAME:=newsletter}"
 NEWSLETTER_DB_PORT="${NEWSLETTER_DB_PORT:=9000}"
 NEWSLETTER_DB_URL="postgres://${NEWSLETTER_DB_USER}:${NEWSLETTER_DB_PASSWORD}@localhost:${NEWSLETTER_DB_PORT}/${NEWSLETTER_DB_NAME}"
 
-if false; then
-docker run \
+POSTGRES_CONTAINER_ID=$(docker run \
   -e POSTGRES_USER="${NEWSLETTER_DB_USER}" \
   -e POSTGRES_PASSWORD="${NEWSLETTER_DB_PASSWORD}" \
   -e POSTGRES_NAME="${NEWSLETTER_DB_NAME}" \
   -e POSTGRES_PORT="${NEWSLETTER_DB_PORT}" \
   -p "${NEWSLETTER_DB_PORT}":5432 \
   -d postgres \
-  postgres -N 1000
-fi
+  postgres -N 1000)
+
+echo >&2 "started postgres container with id: ${POSTGRES_CONTAINER_ID}"
+
+# wait postgres to be ready to accept commands
+while ! docker exec "${POSTGRES_CONTAINER_ID}" pg_isready -U postgres; do
+  echo >&2 "postgres is still unavailable - sleeping"
+  sleep 1
+done
+echo >&2 "postgres is up and running on port ${NEWSLETTER_DB_PORT}!"
 
 DATABASE_URL="${NEWSLETTER_DB_URL}" sqlx database create
