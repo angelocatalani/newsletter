5894b31826e19adc9c0af17312dcf9e6c99c421b
angelocat <catalaniangelo@gmail.com>
2021-05-12 22:31:16 +0200

    feat(db): add migrations for subscription tokens

---
+++ migrations/20210512201254_add_status_to_subscriptions.sql
@@ -0,0 +1,1 @@
+ALTER TABLE subscriptions ADD column status TEXT NULL;
---
+++ migrations/20210512203435_create_subscription_tokens_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE subscription_tokens
+(
+    subscription_token TEXT NOT NULL UNIQUE,
+    subscriber_id uuid NOT NULL REFERENCES subscriptions (id)
+);
